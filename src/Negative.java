2eed19dfc490699eccbf7d2dca34f1dd51f80f5f
rudzzz <rudsolza13@gmail.com>
2022-10-02 11:26:33 -0300

    files

--- src/Negative.java
+++ src/Negative.java
@@ -1,4 +1,16 @@
-
+import java.util.Scanner;
 public class Negative {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = scanner.nextInt();
+		scanner.close();
+		
+		if(number < 0) {
+			System.out.println("Negative number!");
+		}
+		else {
+			System.out.println("Positive Number!");
+		}
+	}
 }
